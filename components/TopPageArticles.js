7a1d3290d41648aa1810817c42c83578f1b70c8c
Ryo Kawakami <rkganma@gmail.com>
2019-08-27 02:29:20 +0900

    トップページ記事ホバー時のスタイル追加

--- components/TopPageArticles.js
+++ components/TopPageArticles.js
@@ -3,6 +3,7 @@ import articles from '../data/listArticles'
 import { Card, Heading, Text } from 'rebass'
 import Link from 'next/link'
 
+
 export default (props) => (
   <>
     <Link href="/articles/test">
@@ -17,7 +18,17 @@ export default (props) => (
 
     {articles.slice(0, 2).map((post, i) => (
       <Link href={`/articles/${post.address}`}>
-        <Card width={[320, 480]} mx="auto" bg="white" p={4} mb={4}>
+        <Card 
+          width={[320, 480]} mx="auto" bg="white" p={4} mb={4} key={i}
+          sx={{
+            boxShadow: '0 0 8px rgba(0, 0, 0, .25)',
+            transition: 'box-shadow .8s',
+            ':hover': {
+              cursor: 'pointer',
+              boxShadow: '0 0 16px rgba(0, 0, 0, .25)',
+            }
+          }}
+        >
           <Heading fontSize={[3,4]} lineHeight={1.25} pb={3} color='rgb(69, 66, 65)'>{post.title}</Heading>
           <Text fontSize={[2,3]} lineHeight={1.25} pb={4} color='rgb(69, 66, 65)'>
             {post.summary}
