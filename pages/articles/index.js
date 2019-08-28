4f1804138b6993008343b10827f040455a468d12
Ryo Kawakami <rkganma@gmail.com>
2019-08-28 17:39:53 +0900

    ブログページを作成

--- pages/articles/index.js
+++ pages/articles/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import Head from 'next/head'
+import TopTitle from '../../components/Toptitle'
+import TopLead from '../../components/TopLead'
+import AllArticles from '../../components/AllArticles'
+
+const blogTitleTop = 'ウバ活する | Articles'
+const blogTitle = 'ウバ活する'
+const blogTitleLead = 'Ubereatsの配達で気づいた事をお届けします。'
 
 const Index = () => (
   <div>
-    {articles.slice(0, 3).map((post, i) => (
-      <p>{post.title}</p>
-    ))}
+    <Head>
+      <title>{blogTitleTop}</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <TopTitle>{blogTitle}</TopTitle>
+    <TopLead>{blogTitleLead}</TopLead>
+    <AllArticles />
   </div>
 );
 
